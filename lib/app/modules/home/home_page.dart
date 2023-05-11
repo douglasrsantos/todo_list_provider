3d58f0bcd9fd33fe1b3ac210170c432bc57da3cd
Douglas Santos <douglasrosasantos@gmail.com>
2023-05-11 18:44:34 -0300

    finished home_page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_tasks.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_week_filter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,20 +15,52 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: context.primaryColor),
-        backgroundColor: Colors.transparent,
+        backgroundColor: const Color(0xFFFAFBFE),
         elevation: 0,
         actions: [
           PopupMenuButton(
             icon: const Icon(TodoListIcons.filter),
             itemBuilder: (_) => const [
-              PopupMenuItem<bool>(child: Text('Mostrar tarefas concluídas')),
+              PopupMenuItem<bool>(
+                child: Text('Mostrar tarefas concluídas'),
+              ),
             ],
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: context.primaryColor,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
       backgroundColor: const Color(0xFFFAFBFE),
       drawer: HomeDrawer(),
-      body: Container(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      HomeHeader(),
+                      HomeFilters(),
+                      HomeWeekFilter(),
+                      HomeTasks(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
